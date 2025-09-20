316691de514059c22c6e1e3a2809b93aa0665c7e
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-09-20 09:16:14 +0800

    chore: setup fonts and themes

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,8 +1,8 @@
 
 const About = () => {
     return (
-        <section className="w-full min-h-screen bg-yellow-400">
-            About
+        <section className="w-full min-h-screen bg-black">
+            <h1 className={"header-gradient text-[6rem] font-roma"}>About Test</h1>
         </section>
     );
 };
