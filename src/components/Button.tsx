ee1a18aebfbf645e348a46e524cb37634f365487
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-09-21 10:37:30 +0800

    fix: adjust hero text and button sizing

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,9 @@ import { type ReactNode } from "react";
 
 const Button = ({children, href}: {children: ReactNode, href: string}) => {
     return (
-        <button className="font-roma text-white text-center text-[1rem} lg:text-[2rem] py-4 px-8 border-1 border-white rounded-3xl hover:bg-white/10 transition-colors duration-300">
-            <a href={href}>{children}</a>
-        </button>
+        <a href={href} className="font-roma text-white text-center text-[1rem] lg:text-[1.5rem] py-4 px-8 border-1 border-white rounded-3xl hover:bg-white/10 transition-colors duration-300">
+            {children}
+        </a>
     );
 };
 
