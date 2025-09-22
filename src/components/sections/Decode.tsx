01fb4ec5aa6f94b5b66ecdbd576889036a86ad03
ChocoCodes <chocojhong999@gmail.com>
2025-09-22 23:02:18 +0800

    fix: sm screens decode

--- src/components/sections/Decode.tsx
+++ src/components/sections/Decode.tsx
@@ -12,11 +12,11 @@ const Decode = () => {
                 className="absolute inset-0 w-full h-full object-cover opacity-35"
             />
             <div className="flex flex-col items-center justify-center gap-20">
-                <img src={ hints } alt="Hint Table" className='w-1/2 h-1/2'/>
-                <div className="z-10 flex flex-col gap-5 text-white font-reddit w-1/2 mx-auto">
-                    <p className='text-xl'>Not everything in this website is as it seems.</p>
+                <img src={ hints } alt="Hint Table" className='w-full md:w-1/2 h-1/2'/>
+                <div className="z-10 flex flex-col gap-5 text-white font-reddit w-full mx-auto">
+                    <p className='text-lg md:text-xl'>Not everything in this website is as it seems.</p>
                     <p className='bg-black/70 border-2 border-white/15 text-3xl p-5 rounded-xl text-center'>{ str }</p>
-                    <p className='text-xl'>The first to comment the decoded message on the website to our <span className='font-bold underline'> 2025 official website posting reveal</span> post will win a <span className="font-bold">free ticket</span> inside our booth.</p>
+                    <p className='text-lg md:text-xl'>The first to comment the decoded message on the website to our <span className='font-bold underline'> 2025 official website posting reveal</span> post will win a <span className="font-bold">free ticket</span> inside our booth.</p>
                 </div>
             </div>
         </section>
