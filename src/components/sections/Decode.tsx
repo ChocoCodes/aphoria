7ed948b9639dd90612fee5650038d98053145d35
ChocoCodes <chocojhong999@gmail.com>
2025-09-24 22:52:06 +0800

    chore: add free ticket text

--- src/components/sections/Decode.tsx
+++ src/components/sections/Decode.tsx
@@ -20,10 +20,10 @@ const Decode = () => {
                 viewport={{ once: true, amount: 0.5 }}
             >
                 <img src={ hints } alt="Hint Table" className='w-full md:w-1/2 h-1/2'/>
-                <div className="z-10 flex flex-col gap-5 text-white font-reddit w-full mx-auto">
+                <div className="z-10 flex flex-col gap-5 text-white font-reddit w-full mx-auto px-4 md:px-8">
                     <p className='text-lg md:text-xl'>Not everything in this website is as it seems.</p>
                     <p className='bg-black/70 border-2 border-white/15 text-3xl p-5 rounded-xl text-center'>{ str }</p>
-                    <p className='text-lg md:text-xl'>The first to comment the decoded message on the website to our <span className='font-bold underline'> 2025 official website posting reveal</span> post will win a <span className="font-bold">free ticket</span> inside our booth.</p>
+                    <p className='text-lg md:text-xl'>The <span className="font-bold underline">first three (3)</span> to comment the decoded message on the website to our <span className='font-bold underline'><a href="https://www.facebook.com/photo/?fbid=1112482047761640&set=a.384589173884268" target='_blank'>2025 official website posting reveal</a></span> post will win a <span className="font-bold">free ticket</span> inside our booth.</p>
                 </div>
             </motion.div>
         </section>
